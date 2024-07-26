baf73861a082c23e56d351b3f49abd528c89067f
yoonsla <sy.kim@twolinecode.com>
2024-07-26 10:29:57 +0900

    first commit

---
+++ src/main/java/com/sy/springsecuritytoken/SpringSecurityTokenApplication.java
@@ -0,0 +1,13 @@
+package com.sy.springsecuritytoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityTokenApplication.class, args);
+    }
+
+}
